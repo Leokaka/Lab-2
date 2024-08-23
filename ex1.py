87c9ec6320fbdbc811d3636efaa35aaeb24b3879
leokaka59 <leokaka59@gmail.com>
2024-08-23 08:45:35 -0300

    Solicitação do nome

--- ex1.py
+++ ex1.py
@@ -1,6 +1,6 @@
 print("Hello Word!")
 
-
+nome = input("Qual é o seu nome? ")
 x = input("Numero: ")
 
-print(f'O numero escolhido é {x}')
+print(f'O seu nome é {nome} e o seu numero escolhido é {x}')
